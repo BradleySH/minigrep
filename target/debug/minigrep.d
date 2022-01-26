5fdbe2f6ba4743efced967146df7761d27bd4a1a
Bradley Horlander <78397249+BradleySH@users.noreply.github.com>
2022-01-25 22:08:16 -0500

    running into issues with importing minigrep and Config as a use statement

--- target/debug/minigrep.d
+++ target/debug/minigrep.d
@@ -1,1 +1,1 @@
-/Users/bradleyhorlander/rust-projects/minigrep/target/debug/minigrep: /Users/bradleyhorlander/rust-projects/minigrep/src/main.rs
+/Users/bradleyhorlander/rust-projects/minigrep/target/debug/minigrep: /Users/bradleyhorlander/rust-projects/minigrep/src/lib.rs /Users/bradleyhorlander/rust-projects/minigrep/src/main.rs
